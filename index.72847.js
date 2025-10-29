8ad033d26c369f61ec276f7f41a8d5a31b368edd
fengqingyang <fengqingyang2022@gmail.com>
2025-10-29 19:42:42 +0800

    1

--- index.72847.js
+++ index.72847.js
@@ -1,4 +1,4 @@
-System.register(["./application.a3dce.js"], function (_export, _context) {
+System.register(["./application.be13c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, timer, percentJd, application;
